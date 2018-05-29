4caea179961949aabdaa7d47ad4efbda31f2e2da
Matus Zamborsky <zamborsky@gmail.com>
2018-05-29 10:28:02 +0200

    fix restClient tests

--- test/restClient.test.ts
+++ test/restClient.test.ts
@@ -1,17 +1,36 @@
+import { PrivateKey } from '../src/crypto';
 import { Address } from '../src/crypto/address';
 import RestClient from '../src/network/rest/restClient';
-import { buildGetDDOTx } from '../src/smartcontract/ontidContractTxBuilder';
+import { WebsocketClient } from '../src/network/websocket/websocketClient';
+import { buildGetDDOTx, buildRegisterOntidTx } from '../src/smartcontract/ontidContractTxBuilder';
+import { signTransaction } from '../src/transaction/transactionBuilder';
 
 // tslint:disable:no-console
 describe('test restClient', () => {
     const rest = new RestClient();
     // tslint:disable-next-line:one-variable-per-declaration
-    const txHash = 'd5200d614994ea5242462f3a6601134ef235b9be03b6ce2f39e871fec2c36768',
-        blockHash = '9d51e95d4cc0365b3ed06f66c5df4808491c09723810cc28ad37d5be152f230b',
-        codeHash = 'ff00000000000000000000000000000000000003',
-        height = 1000,
-        ontid = 'did:ont:TA7j42nDdZSyUBdYhWoxnnE5nUdLyiPoK3',
-        address = 'TA5k9pH3HopmscvgQYx8ptfCAPuj9u2HxG';
+    const codeHash = 'ff00000000000000000000000000000000000003';
+    const ontid = 'did:ont:TGpoKGo26xmnA1imgLwLvYH2nhWnN62G9w';
+    const address = 'TA5k9pH3HopmscvgQYx8ptfCAPuj9u2HxG';
+
+    let txHash: string;
+    let blockHash: string;
+    let height: number;
+
+    const privateKey = new PrivateKey('eaec4e682c93648d24e198da5ef9a9252abd5355c568cd74fba59f98c0b1a8f4');
+    const publicKey = privateKey.getPublicKey();
+
+    /**
+     * Registers new ONT ID to create transaction with Events and new block
+     */
+    beforeAll(async () => {
+        const tx = buildRegisterOntidTx(ontid, publicKey, '0', '30000');
+        signTransaction(tx, privateKey);
+
+        const client = new WebsocketClient();
+        const result = await client.sendRawTransaction(tx.serialize(), false, true);
+        txHash = result.Result.TxHash;
+    }, 10000);
 
     test('test sendRawTransaction', async () => {
         const tx = buildGetDDOTx(ontid);
@@ -44,17 +63,15 @@ describe('test restClient', () => {
         expect(res.Result).toBeTruthy();
     });
 
-    test('test getBlockHeight', () => {
-        return rest.getBlockHeight().then((data) => {
-            console.log(data);
-            expect(data).toBeDefined();
-        });
-    });
+    /**
+     * Gets current block height to be used by following tests.
+     */
+    test('test getBlockHeight', async () => {
+        const response = await rest.getBlockHeight();
 
-    test('test getBlock by hash', async () => {
-        const res = await rest.getBlock(blockHash);
-        console.log(res);
-        expect(res.Result).toBeTruthy();
+        expect(response).toBeDefined();
+        expect(response.Result).toBeDefined();
+        height = response.Result;
     });
 
     test('test getBlock by height', async () => {
@@ -63,14 +80,25 @@ describe('test restClient', () => {
         expect(res.Result).toBeTruthy();
     });
 
-    test('test getBlockJson by hash', async () => {
-        const res = await rest.getBlockJson(blockHash);
+    /**
+     * Gets block hash to be used by following tests.
+     */
+    test('test getBlockJson by height', async () => {
+        const res = await rest.getBlockJson(height);
+        console.log(res);
+        expect(res.Result).toBeTruthy();
+        expect(res.Result.Hash).toBeDefined();
+        blockHash = res.Result.Hash;
+    });
+
+    test('test getBlock by hash', async () => {
+        const res = await rest.getBlock(blockHash);
         console.log(res);
         expect(res.Result).toBeTruthy();
     });
 
-    test('test getBlockJson by height', async () => {
-        const res = await rest.getBlockJson(height);
+    test('test getBlockJson by hash', async () => {
+        const res = await rest.getBlockJson(blockHash);
         console.log(res);
         expect(res.Result).toBeTruthy();
     });
@@ -93,13 +121,13 @@ describe('test restClient', () => {
         expect(res.Result).toBeTruthy();
     });
 
-    test.skip('test smartCodeEvent by hash', async () => {
+    test('test smartCodeEvent by hash', async () => {
         const res = await rest.getSmartCodeEvent(txHash);
         console.log(res);
         expect(res.Result).toBeTruthy();
     });
 
-    test.skip('test smartCodeEvent by height', async () => {
+    test('test smartCodeEvent by height', async () => {
         const res = await rest.getSmartCodeEvent(height);
         console.log(res);
         expect(res.Result).toBeTruthy();
