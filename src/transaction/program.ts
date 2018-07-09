a5a2a0227ad22762a80791625b3086927804bb7d
MickeyWang <wangdashuang@onchain.com>
2018-07-09 19:38:32 +0800

    export struct,scriptbuilder

--- src/transaction/program.ts
+++ src/transaction/program.ts
@@ -16,10 +16,53 @@
  * along with The ontology.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import * as elliptic from 'elliptic';
+import { sm2 } from 'sm.js';
 import BigInt from '../common/bigInt';
+import { KeyType } from '../crypto/KeyType';
 import { PublicKey } from '../crypto/PublicKey';
 import { num2hexstring, StringReader } from './../utils';
 import opcode from './opcode';
+// The sorting rules is as follows:
+//    1. if keys have different types, then sorted by the KeyType value.
+//    2. else,
+//       2.1. ECDSA or SM2:
+//           2.1.1. if on different curves, then sorted by the curve label.
+//           2.1.2. else if x values are different, then sorted by x.
+//           2.1.3. else sorted by y.
+//       2.2. EdDSA: sorted by the byte sequence directly.
+export function comparePublicKeys(a: PublicKey, b: PublicKey) {
+    if (a.algorithm !== b.algorithm) {
+        return a.algorithm.hex - b.algorithm.hex;
+    }
+    switch (a.algorithm) {
+    case KeyType.ECDSA:
+        const ec = new elliptic.ec(a.parameters.curve.preset);
+        const paKey = ec.keyFromPublic(a.key, 'hex', true);
+        const pbKey = ec.keyFromPublic(b.key, 'hex', true);
+        const pa = paKey.getPublic();
+        const pb = pbKey.getPublic();
+        if (pa.getX() !== pb.getX()) {
+            return pa.getX() - pb.getX();
+        } else {
+            return pa.getY() - pb.getY();
+        }
+    case KeyType.SM2:
+        const pka = new sm2.SM2KeyPair();
+        const pkb = new sm2.SM2KeyPair();
+        pka._pubFromString(a.key);
+        pkb._pubFromString(b.key);
+        if (pka.getX().toString() !== pkb.getX().toString()) {
+            return Number(pka.getX().toString()) - Number(pkb.getX().toString());
+        } else {
+            return Number(pka.getY().toString()) - Number(pkb.getY().toString());
+        }
+    case KeyType.EDDSA:
+        return Number(a.key) - Number(b.key);
+    default:
+        return 0;
+    }
+}
 
 export function pushOpCode(op: opcode): string {
     return num2hexstring(op);
@@ -87,13 +130,16 @@ export function programFromMultiPubKey(pubkeys: PublicKey[], m: number): string 
     if (!(1 <= m && m <= n && n <= 1024)) {
         throw new Error('Wrong multi-sig param');
     }
-    const pkStrList = pubkeys.map( (p) => p.serializeHex());
-    pkStrList.sort();
+    // const pkStrList = pubkeys.map( (p) => p.serializeHex());
+    // pkStrList.sort();
+
+    pubkeys.sort(comparePublicKeys);
 
     let result = '';
     result += pushNum(m);
-    for (const pk of pkStrList) {
-        result += pushBytes(pk);
+
+    for (const pk of pubkeys) {
+        result += pushBytes(pk.serializeHex());
     }
     result += pushNum(n);
     result += pushOpCode(opcode.CHECKMULTISIG);
@@ -150,7 +196,7 @@ export function readBytes(sr: StringReader) {
 }
 
 export function readPubKey(sr: StringReader) {
-    const pkStr = readBytes(sr);
+    const pkStr = sr.readNextBytes();
     return PublicKey.deserializeHex(new StringReader(pkStr));
 }
 
@@ -178,10 +224,11 @@ export function getProgramInfo(hexstr: string): ProgramInfo {
         return info;
     } else if (end === opcode.CHECKMULTISIG) {
         const sr = new StringReader(hexstr);
-        const m = readNum(sr);
+        const m = parseInt(sr.read(1), 16) - opcode.PUSH1 + 1;
+        const n = parseInt(hexstr.substr(-4, 2), 16) - opcode.PUSH1 + 1;
         info.M = m;
         info.pubKeys = [];
-        for (let i = 0; i < m; i++) {
+        for (let i = 0; i < n; i++) {
             const key = readPubKey(sr);
             info.pubKeys.push(key);
         }
