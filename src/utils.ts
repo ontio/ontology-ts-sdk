9377df4324ff478f678ee241d0a5df424743cb65
mpomykalski <pomykalskimateusz@gmail.com>
2020-09-29 09:53:15 +0200

    create tests for jwt header

--- src/utils.ts
+++ src/utils.ts
@@ -708,3 +708,20 @@ export function calcUnboundOng(balance: number, startOffset: number, endOffset: 
     }
     return amount * balance;
 }
+
+export function isEqual(a: any, b: any) {
+    let aProps = Object.getOwnPropertyNames(a);
+    let bProps = Object.getOwnPropertyNames(b);
+
+    if (aProps.length != bProps.length) {
+        return false;
+    }
+    for (let i = 0; i < aProps.length; i++) {
+        let propName = aProps[i];
+
+        if (a[propName] !== b[propName]) {
+            return false;
+        }
+    }
+    return true;
+}
