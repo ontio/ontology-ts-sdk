fe9a42b87421e76a4d6dd97c75f05d0186128bb6
mpomykalski <pomykalskimateusz@gmail.com>
2020-09-30 11:40:44 +0200

    move isEqual function to test util

--- test/credentials/util-functions.ts
+++ test/credentials/util-functions.ts
@@ -11,3 +11,20 @@ export function assertPayload(jwtPayload: JwtPayload, deserialized: JwtPayload) 
 export function _isNotEmpty(value: string): Boolean {
     return value.length > 0;
 }
+
+export function isEqual(a: any, b: any) {
+    const aProps = Object.getOwnPropertyNames(a);
+    const bProps = Object.getOwnPropertyNames(b);
+
+    if (aProps.length !== bProps.length) {
+        return false;
+    }
+    for (let i = 0; i < aProps.length; i++) {
+        const propName = aProps[i];
+
+        if (a[propName] !== b[propName]) {
+            return false;
+        }
+    }
+    return true;
+}
